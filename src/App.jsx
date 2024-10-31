702c9f23ec561d9b001e50d8979b1f983088a67f
enesseval <enesseval@outlook.com>
2024-10-31 14:38:16 +0300

    init, header and hero section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Features from "./sections/Features";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+
+function App() {
+   return (
+      <main className="overflow-hidden">
+         <Header />
+         <Hero />
+         <Features />
+      </main>
+   );
+}
+
+export default App;
